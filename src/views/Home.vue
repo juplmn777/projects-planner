06e790314cc3c1d96011ebe8664a47b29e2fa20a
juplmn777 <julien.plumyoen@gmail.com>
2021-12-19 11:55:55 +0100

    dats fetched & used in homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,33 @@
 <template>
-  <div class="home">Home Page</div>
+  <div class="home">
+    <div v-if="projects.length">
+      <div v-for="project in projects" :key="project.id">
+        <p>{{ project.title }}</p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'Home',
-  components: {
-
+  components: {},
+  data() {
+    return {
+      projects: [],
+    };
+  },
+  mounted() {
+    fetch('http://localhost:3000/projects')
+      .then((response) => response.json())
+      .then((data) => (this.projects = data))
+      .catch((error) => console.log(error.message));
   },
 };
 </script>
 
 <style scoped>
-  .home{
-    font-size: 1.5rem;
-  }
+.home {
+  font-size: 1.5rem;
+}
 </style>
