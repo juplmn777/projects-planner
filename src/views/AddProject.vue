14218340608b1af5987208f4d45d2eeff64531fb
juplmn777 <julien.plumyoen@gmail.com>
2021-12-23 10:32:16 +0100

    Adding project ok

--- src/views/AddProject.vue
+++ src/views/AddProject.vue
@@ -18,7 +18,21 @@ export default {
   },
   methods: {
     handleSubmit() {
-      console.log(this.title, this.details);
+      let project = {
+        title: this.title,
+        details: this.details,
+        complete: false,
+      };
+
+      fetch('http://localhost:3000/projects', {
+        method: 'POST',
+        headers: { 'Content-type': 'application/json' },
+        body: JSON.stringify(project),
+      })
+        .then(() => {
+          this.$router.push('/');
+        })
+        .catch((error) => console.log(error));
     },
   },
 };
